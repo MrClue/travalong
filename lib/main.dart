1e807094519ca9385a9ebc0a77bd486688d73698
MrClue <gayssuck123@gmail.com>
2022-11-18 13:25:16 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:travalong/presentation/profile_screens/profile_main.dart';
 
 void main() {
   //runApp(const MyApp());
-  runApp(const ProfileMain());
+  runApp(const MyProfile());
 }
