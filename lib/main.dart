77b811ad923aafdaa678f1e57a6ed40a062cafc7
MrClue <gayssuck123@gmail.com>
2022-11-19 02:37:23 +0100

    restructure main.dart and refactorings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:travalong/presentation/profile_screens/profile_main.dart';
+import 'package:travalong/presentation/profile_screens/profile_page.dart';
 
 void main() {
-  //runApp(const MyApp());
-  runApp(const MyProfile());
+  runApp(const TravalongApp());
+}
+
+class TravalongApp extends StatelessWidget {
+  const TravalongApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'TRAVALONG',
+      home:
+          ProfilePage(), // ! route that is displayed first when app starts (unless "initialRoute" is specified)
+    );
+  }
 }
