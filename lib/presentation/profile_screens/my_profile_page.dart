9cfdf7d11a05c05bbe81ad66f0ec1ccd57962a6a
MrClue <gayssuck123@gmail.com>
2022-11-22 13:40:54 +0100

    .

--- lib/presentation/profile_screens/my_profile_page.dart
+++ lib/presentation/profile_screens/my_profile_page.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../resources/colors.dart';
 
 class MyProfilePage extends StatelessWidget {
   const MyProfilePage({super.key});
@@ -6,9 +9,19 @@ class MyProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Profile"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("My Profile"),
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                FaIcon(
+                  FontAwesomeIcons.magnifyingGlassPlus,
+                  size: 35,
+                  color: TravalongColors.secondary_10,
+                ),
+              ]),
+        ));
   }
 }
