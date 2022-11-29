75d21de4699cdee3e19aa939bf1889254a96858b
MrClue <gayssuck123@gmail.com>
2022-11-29 20:15:15 +0100

    Added gesture support for app + exporting screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travalong/presentation/profile_screens/profile_page.dart';
-//import 'package:travalong/presentation/profile_screens/profile_page.dart';
-import 'package:travalong/presentation/start_screens/start_screen.dart';
+import 'package:travalong/presentation/screens.dart';
 
 void main() {
   runApp(const TravalongApp());
@@ -11,6 +9,37 @@ class TravalongApp extends StatelessWidget {
   const TravalongApp({super.key});
 
   // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    // * wrapping top of widget tree with a "gesture detector",
+    // * to dismiss keyboard when clicking away from a textfield.
+    // inspiration: https://flutterigniter.com/dismiss-keyboard-form-lose-focus/
+
+    return GestureDetector(
+      onTap: () {
+        // * We need to remove “focus” from our text field, for the keyboard to dismiss itself.
+        // * This is done by removing current focus from the text fields "FocusNode".
+        FocusScopeNode currentFocus =
+            FocusScope.of(context); // gets current FocusNode
+
+        // check if the current FocusNode has the “primary focus.”
+        // ! prevents exception, when trying to unfocus the node at the top of the tree
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          //currentFocus.unfocus();
+          FocusManager.instance.primaryFocus!.unfocus(); // cant be null
+        }
+      },
+      child: const MaterialApp(
+        title: 'TRAVALONG',
+        home:
+            ProfilePage(), // * route that is displayed first when app starts (unless "initialRoute" is specified)
+      ),
+    );
+  }
+
+  /*
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -19,4 +48,5 @@ class TravalongApp extends StatelessWidget {
           ProfilePage(), // ! route that is displayed first when app starts (unless "initialRoute" is specified)
     );
   }
+  */
 }
