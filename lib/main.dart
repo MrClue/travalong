b5e2228c42be9bf1bb8af26679df7d49b1273ec4
MrClue <gayssuck123@gmail.com>
2022-12-01 17:15:25 +0100

    kighercommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:travalong/presentation/screens.dart';
 import 'package:travalong/presentation/profile_screens/home_page.dart';
-import 'package:travalong/presentation/resources/widgets/molecules/show_snack_bar.dart';
 import 'package:travalong/presentation/start_screens/auth_page.dart';
-import 'package:travalong/presentation/start_screens/start_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -58,19 +55,19 @@ class TravalongApp extends StatelessWidget {
           body: StreamBuilder(
             stream: FirebaseAuth.instance.authStateChanges(),
             builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshot.hasData) {
-                  return HomePage();
-                } else if (snapshot.hasError) {
-                  return const Center(
-                    child: Text("Something went wrong..."),
-                  );
-                } else {
-                  return AuthPage();
-                }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasData) {
+                return HomePage();
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: Text("Something went wrong..."),
+                );
+              } else {
+                return AuthPage();
+              }
             },
           ),
         ),
