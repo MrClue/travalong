93600f634aefb5e4b872d941655724ba63e20b71
sanjr19 <samuel-njri@hotmail.com>
2022-12-05 22:04:35 +0100

    fix multiselect error

--- lib/presentation/profile_screens/interests_subpage.dart
+++ lib/presentation/profile_screens/interests_subpage.dart
@@ -54,11 +54,11 @@ class InterestsSubpage extends StatelessWidget {
 }
 
 // ! selection items code
-class Interests {
+class Interest {
   final int id;
   final String name;
 
-  Interests({required this.id, required this.name});
+  Interest({required this.id, required this.name});
 }
 
 class InterestsWidget extends StatefulWidget {
@@ -69,21 +69,21 @@ class InterestsWidget extends StatefulWidget {
 }
 
 class _InterestsWidgetState extends State<InterestsWidget> {
-  static List<Interests> _interests = [
-    Interests(id: 1, name: "Interest1"),
-    Interests(id: 2, name: "Interest2"),
-    Interests(id: 3, name: "Interest3"),
-    Interests(id: 4, name: "Interest4"),
-    Interests(id: 5, name: "Interest5"),
-    Interests(id: 6, name: "Interest6"),
-    Interests(id: 7, name: "Interest7"),
-    Interests(id: 8, name: "Interest8"),
+  static final List<Interest> _interests = [
+    Interest(id: 1, name: "Interest1"),
+    Interest(id: 2, name: "Interest2"),
+    Interest(id: 3, name: "Interest3"),
+    Interest(id: 4, name: "Interest4"),
+    Interest(id: 5, name: "Interest5"),
+    Interest(id: 6, name: "Interest6"),
+    Interest(id: 7, name: "Interest7"),
+    Interest(id: 8, name: "Interest8"),
   ];
   final _items = _interests
-      .map((interest) => MultiSelectItem(interest, interest.name))
+      .map((interest) => MultiSelectItem<Interest>(interest, interest.name))
       .toList();
 
-  List<Interests> _selectedInterests = [];
+  List<Interest> _selectedInterests = [];
   final _multiSelectKey = GlobalKey<FormFieldState>();
 
   @override
@@ -94,57 +94,59 @@ class _InterestsWidgetState extends State<InterestsWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 40),
-              Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor.withOpacity(.4),
-                  border: Border.all(
-                    color: Theme.of(context).primaryColor,
-                    width: 2,
-                  ),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    MultiSelectBottomSheetField(
-                      initialChildSize: 0.4,
-                      listType: MultiSelectListType.CHIP,
-                      searchable: true,
-                      buttonText: const Text("Your Interests"),
-                      title: const Text("Interests"),
-                      items: _items,
-                      onConfirm: (values) {
-                        _selectedInterests = values;
-                      },
-                      chipDisplay: MultiSelectChipDisplay(
-                        onTap: (value) {
-                          setState(() {
-                            _selectedInterests.remove(value);
-                          });
-                        },
-                      ),
-                    ),
-                    _selectedInterests == null || _selectedInterests.isEmpty
-                        ? Container(
-                            padding: const EdgeInsets.all(10),
-                            alignment: Alignment.centerLeft,
-                            child: const Text(
-                              "None selected",
-                              style: TextStyle(color: Colors.black54),
-                            ))
-                        : Container(),
-                  ],
+    return SingleChildScrollView(
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 40),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor.withOpacity(.4),
+                border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                  width: 2,
                 ),
               ),
-              const SizedBox(height: 40),
-            ],
-          ),
+              child: Column(
+                children: <Widget>[
+                  MultiSelectBottomSheetField(
+                    initialChildSize: 0.4,
+                    listType: MultiSelectListType.CHIP,
+                    searchable: true,
+                    buttonText: const Text("Your Interests"),
+                    title: const Text("Interests"),
+                    items: _items,
+                    onConfirm: (values) {
+                      List<Interest> bufferSelectedInterest = [];
+                      var newValues = values as List<MultiSelectItem<Interest>>;
+                      newValues.forEach((element) {
+                        bufferSelectedInterest.add(element.value);
+                      });
+                    },
+                    chipDisplay: MultiSelectChipDisplay(
+                      onTap: (value) {
+                        setState(() {
+                          _selectedInterests.remove(value);
+                        });
+                      },
+                    ),
+                  ),
+                  _selectedInterests.isEmpty
+                      ? Container(
+                          padding: const EdgeInsets.all(10),
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            "None selected",
+                            style: TextStyle(color: Colors.black54),
+                          ))
+                      : Container(),
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+          ],
         ),
       ),
     );
