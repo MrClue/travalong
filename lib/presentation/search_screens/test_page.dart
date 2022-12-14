eda493d17eb139c962dfdcba7a4b2d6243b333bc
MrClue <Motar19@student.sdu.dk>
2022-12-14 16:01:59 +0100

    more

--- lib/presentation/search_screens/test_page.dart
+++ lib/presentation/search_screens/test_page.dart
@@ -3,10 +3,10 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:travalong/presentation/resources/widgets/atoms/safe_scaffold.dart';
 
-// * STEP 1: Hent current user fra firebase
-// * STEP 2: Få fat i current users "age"
-// * STEP 3: Update current users "age" i DB
-// * STEP 4: Få fat i den opdateret current_users "age" og print i Text()
+// * STEP 1: Hent current_user fra firebase (getter)
+// * STEP 2: Få fat i current_users "age" (getter)
+// * STEP 3: Update current_users "age" i DB (setter)
+// * STEP 4: Få fat i den opdateret current_users "age" og print i Text() (getter)
 
 class TestPage extends StatelessWidget {
   const TestPage({super.key});
