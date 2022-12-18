3839f2f504c4ce763ffb256974e87900d3ac1502
MrClue <Motar19@student.sdu.dk>
2022-12-18 03:09:34 +0100

    added conditional options to constructors

--- lib/presentation/resources/widgets/atoms/theme_container.dart
+++ lib/presentation/resources/widgets/atoms/theme_container.dart
@@ -3,20 +3,22 @@ import 'package:flutter/material.dart';
 import '../../colors.dart';
 
 class ThemeContainer extends StatelessWidget {
-  final double height;
-  //final double width;
+  final double height, width;
+  final bool customWidth;
   final Widget child;
 
   const ThemeContainer({
     super.key,
     required this.height,
     required this.child,
+    this.customWidth = false,
+    this.width = 100,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width - 28,
+      width: customWidth ? width : MediaQuery.of(context).size.width - 28,
       height: height,
       padding: const EdgeInsets.only(left: 10, right: 10),
       decoration: BoxDecoration(
