8f103ed7e42f6f5a3a583e8a6cd431556b730de4
sanjr19 <samuel-njri@hotmail.com>
2022-12-20 00:07:06 +0100

    add slideup panel

--- lib/presentation/screens/search/search_start_screen.dart
+++ lib/presentation/screens/search/search_start_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:travalong/presentation/resources/colors.dart';
 import 'package:travalong/presentation/resources/widgets/atoms/safe_scaffold.dart';
 import 'package:travalong/presentation/resources/widgets/atoms/theme_text.dart';
 import 'package:travalong/presentation/resources/widgets/molecules/search_bar.dart';
 import 'package:travalong/presentation/resources/widgets/molecules/theme_topbar.dart';
-import 'package:travalong/presentation/travalong_navbar.dart';
 import 'package:intl/intl.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class SearchStartScreen extends StatefulWidget {
   const SearchStartScreen({super.key});
@@ -15,6 +16,7 @@ class SearchStartScreen extends StatefulWidget {
 }
 
 const List<String> list = <String>['Male', 'Female', 'Any'];
+const List<String> optList = <String>['currently in', 'going to'];
 
 class _SearchStartScreenState extends State<SearchStartScreen> {
   DateTimeRange dateRange = DateTimeRange(
@@ -24,6 +26,7 @@ class _SearchStartScreenState extends State<SearchStartScreen> {
 
   TextEditingController dateController = TextEditingController();
   String dropdownValue = list.first;
+  String optvalue = optList.first;
 
   @override
   Widget build(BuildContext context) {
@@ -32,26 +35,41 @@ class _SearchStartScreenState extends State<SearchStartScreen> {
         title: 'Search Travelers',
         enableCustomButton: false,
       ),
-      child: Container(
-        width: double.infinity,
-        color: TravalongColors.primary_30,
-        child: Column(
-          children: [
-            SizedBox(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SearchBar(),
+      child: SlidingUpPanel(
+        panelSnapping: true,
+        isDraggable: true,
+        backdropEnabled: true,
+        minHeight: MediaQuery.of(context).size.height / 2 - 50,
+        maxHeight: MediaQuery.of(context).size.height - 250,
+        panel: Center(
+          child: Text("This is the sliding Widget"),
+        ),
+        collapsed: Text('This is the collapsed panel'),
+        body: Container(
+          width: double.infinity,
+          color: TravalongColors.primary_30,
+          child: Column(
+            children: [
+              SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SearchBar(),
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _searchDateWidget(),
-                SizedBox(width: 4),
-                _searchGenderWidget(),
-              ],
-            )
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _searchDateWidget(),
+                  const SizedBox(width: 4),
+                  _searchGenderWidget(),
+                ],
+              ),
+              const SizedBox(height: 25),
+              _searchTextWidget(),
+              const SizedBox(height: 75),
+              _searchActionBtn(),
+            ],
+          ),
         ),
       ),
     );
@@ -68,22 +86,26 @@ class _SearchStartScreenState extends State<SearchStartScreen> {
       decoration: BoxDecoration(
         color: TravalongColors.neutral_60,
         border: Border.all(color: TravalongColors.primary_30_stroke),
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(20),
       ),
       child: Row(
         children: [
           Expanded(
             child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(TravalongColors.secondary_10),
-                textStyle: MaterialStateProperty.all(TextStyle(fontSize: 12)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: TravalongColors.secondary_10,
+                textStyle: const TextStyle(fontSize: 12),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)),
+                ),
               ),
               onPressed: selectDateRange,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.calendar_month_outlined),
+                  const Icon(Icons.calendar_month_outlined),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -96,25 +118,29 @@ class _SearchStartScreenState extends State<SearchStartScreen> {
               ),
             ),
           ),
-          SizedBox(width: 1),
+          const SizedBox(width: 1),
           Expanded(
             child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(TravalongColors.secondary_10),
-                textStyle: MaterialStateProperty.all(TextStyle(fontSize: 12)),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: TravalongColors.secondary_10,
+                textStyle: const TextStyle(fontSize: 12),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                ),
               ),
               onPressed: selectDateRange,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.calendar_month_outlined),
+                  const Icon(Icons.calendar_month_outlined),
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(DateFormat('dd/MM/yyyy').format(start)),
-                      Text(DateFormat('EEEE').format(start)),
+                      Text(DateFormat('dd/MM/yyyy').format(end)),
+                      Text(DateFormat('EEEE').format(end)),
                     ],
                   ),
                 ],
@@ -124,6 +150,39 @@ class _SearchStartScreenState extends State<SearchStartScreen> {
         ],
       ),
     );
+  }
+
+  Future selectDateRange() async {
+    DateTimeRange? newDateRange = await showDateRangePicker(
+        context: context,
+        initialDateRange: dateRange,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100),
+        builder: (context, child) {
+          return Theme(
+            data: Theme.of(context).copyWith(
+              colorScheme: const ColorScheme.light(
+                primary: TravalongColors.secondary_10,
+                onPrimary: TravalongColors.primary_30,
+                onSurface: TravalongColors.primary_text_bright,
+              ),
+              textButtonTheme: TextButtonThemeData(
+                style: TextButton.styleFrom(
+                  foregroundColor: TravalongColors.primary_30,
+                  backgroundColor:
+                      TravalongColors.secondary_10, // button text color
+                ),
+              ),
+            ),
+            child: child!,
+          );
+        });
+
+    if (newDateRange == null) return; // pressed 'X'
+
+    setState(() {
+      dateRange = newDateRange; // pressed 'Saved'
+    });
   }
 
   Widget _searchGenderWidget() {
@@ -178,18 +237,103 @@ class _SearchStartScreenState extends State<SearchStartScreen> {
     );
   }
 
-  Future selectDateRange() async {
-    DateTimeRange? newDateRange = await showDateRangePicker(
-      context: context,
-      initialDateRange: dateRange,
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
+  Widget _searchTextWidget() {
+    return Container(
+      alignment: Alignment.center,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const IntrinsicWidth(
+            child: ThemeText(
+              textString: 'Im looking for people',
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              textColor: TravalongColors.primary_text_bright,
+            ),
+          ),
+          _textOptWidget(),
+          const IntrinsicWidth(
+            child: ThemeText(
+              textString: ' the destination',
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              textColor: TravalongColors.primary_text_bright,
+            ),
+          ),
+        ],
+      ),
     );
+  }
 
-    if (newDateRange == null) return; // pressed 'X'
+  Widget _textOptWidget() {
+    return SizedBox(
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton2(
+          // https://pub.dev/packages/dropdown_button2/example
+          alignment: AlignmentDirectional.center,
+          buttonHighlightColor: Colors.transparent,
+          hint: const ThemeText(
+            textString: '- select option -',
+            fontSize: 12,
+            fontWeight: FontWeight.normal,
+            textColor: TravalongColors.secondary_10,
+          ),
+          iconEnabledColor: Colors.transparent,
+          iconDisabledColor: Colors.transparent,
+          value: optvalue,
+          dropdownElevation: 16,
+          dropdownWidth: 104,
+          dropdownDecoration: const BoxDecoration(
+            color: TravalongColors.primary_30,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          style: const TextStyle(
+            color: TravalongColors.secondary_10,
+          ),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              optvalue = value!;
+            });
+          },
+          items: optList.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
 
-    setState(() {
-      dateRange = newDateRange; // pressed 'Saved'
-    });
+  Widget _searchActionBtn() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: 40,
+        width: 367 + 12,
+        decoration: BoxDecoration(
+          color: TravalongColors.secondary_10,
+          borderRadius: BorderRadius.circular(20.0),
+          border: Border.all(
+            color: TravalongColors.primary_30_stroke, // red as border color
+          ),
+        ),
+        child: const Align(
+          alignment: Alignment.center,
+          child: ThemeText(
+            textString: 'Search',
+            fontSize: 16,
+            fontWeight: FontWeight.normal,
+            textColor: TravalongColors.primary_text_dark,
+          ),
+        ),
+      ),
+    );
   }
 }
