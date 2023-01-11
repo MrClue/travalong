640543baf9cf6a881178c9c49a9d1d0d99c04034
sanjr19 <samuel-njri@hotmail.com>
2023-01-11 16:37:43 +0100

    add working unit test for firebase controller

---
+++ test/firebase_test.dart
@@ -0,0 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:test/test.dart';
+
+FakeFirebaseFirestore? fakeFirebaseFirestore;
+
+void main() {
+  group('FirestoreController Tests', () {
+    setUp(() {
+      fakeFirebaseFirestore = FakeFirebaseFirestore();
+    });
+
+    test('setDocFieldData adds data into a collection (users) where uid match',
+        () async {
+      // Initial state
+      final FakeFirebaseFirestore fakeFirebaseFirestore =
+          FakeFirebaseFirestore();
+
+      const String collectionPath = 'users';
+      const String documentPath = 'uid1';
+      const Map<String, dynamic> data = {
+        'name': 'John Doe',
+      };
+
+      await fakeFirebaseFirestore
+          .collection(collectionPath)
+          .doc(documentPath)
+          .set(data);
+
+      // Final state
+      final DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+          await fakeFirebaseFirestore
+              .collection(collectionPath)
+              .doc(documentPath)
+              .get();
+
+      final Map<String, dynamic> actualData = documentSnapshot.data()!;
+
+      print(actualData);
+
+      // Assertion
+      expect(actualData, data);
+    });
+  });
+  test('getDocData gets data from a doc datafield', () async {
+    // Initial state
+    final FakeFirebaseFirestore fakeFirebaseFirestore = FakeFirebaseFirestore();
+
+    const String collectionPath = 'users';
+    const String documentPath = 'uid';
+    const String field = 'name';
+    const Map<String, dynamic> data = {
+      'name': 'John Doe',
+    };
+
+    await fakeFirebaseFirestore
+        .collection(collectionPath)
+        .doc(documentPath)
+        .set(data);
+
+    // Final state
+    final DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+        await fakeFirebaseFirestore
+            .collection(collectionPath)
+            .doc(documentPath)
+            .get();
+
+    final String actualData = documentSnapshot.get(field);
+
+    print(actualData);
+
+    // Assertion
+    expect(actualData, data[field]);
+  });
+}
